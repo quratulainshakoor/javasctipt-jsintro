a887686137fb828f1d8734f6e861fa547ba88977
Qurat-ul-ain Shakoor <55014098+quratulainshakoor@users.noreply.github.com>
2022-11-07 05:09:15 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+///question 1
+
+var age = 18;
+if(age >= 18){
+    console.log("You are an adult");
+}else{
+    console.log("You are a child");
+}
+
+var age = 14;
+if(age >=18){
+    console.log("You are an adult");
+}else{
+    console.log("You are a child");
+}
+
+
+
+///question 2
+var num = 21;
+if(num % 2 ==0){
+    console.log("the number is even");
+}else{
+    console.log("the number is odd");
+}
+
+//or
+var num = 20;
+if(num % 2 ==0){
+    console.log("the number is even");
+}else{
+    console.log("the number is odd");
+}
+
+
+
+
+///question 3
+
+var game =["annie", "hello", "the", "world", "game"];
+console.log(game.length); 
+console.log(game[0]); 
+console.log(game[1]);
+console.log(game[2]);
+console.log(game[3]);   
+console.log(game[5]); 
+
+///question 4 
+
+var  object = {
+    city1:"karachi",
+    city2:"Islamabad",
+    city3:"lahore",
+    city4:"Quetta",
+    city5:"Rawalpandi"
+}
+
+console.log(object.city1);
+console.log(object.city4);
+
+
+///question 5
+
+// var age = "5" +  "6";
+// console.log(age);
+
+//or
+var age = 'My Age is';
+var str= age +  " " + "22"
+console.log(str);
+
+
+///question6
+var string = 'Javascript';
+var mystrlength = string.length;
+console.log(mystrlength);
+
+
+
